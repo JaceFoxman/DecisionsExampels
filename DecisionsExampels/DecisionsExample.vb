a74056063770c9ecf66add6dd41f2d74f211ae2b
Jason Permann <JaceFoxman@gmail.com>
2025-01-27 08:33:16 -0700

    Add 2 if then exampels

--- DecisionsExampels/DecisionsExample.vb
+++ DecisionsExampels/DecisionsExample.vb
@@ -8,7 +8,35 @@ Option Strict On
 Module DecisionsExample
 
     Sub Main()
+        Dim variable1 As Integer
 
+        'assign 5 to variable1
+        variable1 = 5
+
+        'test is variable1 greater than 6
+        'display the result on console
+        If variable1 > 6 Then
+            Console.WriteLine(variable1 > 6) 'false so it skips 
+        End If
+
+        If variable1 > 3 Then
+            Console.WriteLine(variable1 > 3) 'true so it prints 
+        End If
+
+        If variable1 > 4 Then
+            Console.WriteLine(variable1 > 4) 'true so it prints
+        End If
+
+
+        If variable1 > 6 Then
+            Console.WriteLine(variable1 > 6) 'false so it skips 
+
+        ElseIf variable1 > 3 Then
+            Console.WriteLine(variable1 > 3) 'true so it prints and dosent contune through
+
+        ElseIf variable1 > 4 Then
+            Console.WriteLine(variable1 > 4)
+        End If
     End Sub
 
 End Module
